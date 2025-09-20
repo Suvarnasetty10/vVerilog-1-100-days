c3ec9f9a05b3ce153b023fbfd7c3d73d62e48715
K.Suvarna Subramaneswari <166210217+Suvarnasetty10@users.noreply.github.com>
2025-09-20 13:38:19 +0530

    designcode.v

---
+++ XOR_Gate/designcode.v
@@ -0,0 +1,11 @@
+module xor_gate_using_mux (
+    input wire a,
+    input wire b,
+    output wire y
+);
+    wire I0; 
+    wire I1;
+    assign I0 = b;
+    assign I1 = ~b;
+  assign y = (~a & I0) | (a & I1);
+endmodule
