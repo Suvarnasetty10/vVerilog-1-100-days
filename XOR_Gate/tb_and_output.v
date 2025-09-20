f1cf16ca35df0bccec560677454c1fff3f93c526
K.Suvarna Subramaneswari <166210217+Suvarnasetty10@users.noreply.github.com>
2025-09-20 13:39:32 +0530

    tb_and_output.v

---
+++ XOR_Gate/tb_and_output.v
@@ -0,0 +1,22 @@
+module tb_xor_gate_using_mux;
+    reg a, b;
+    wire y;
+    xor_gate_using_mux uut (
+      .a(a),
+      .b(b),
+      .y(y)
+    );
+    initial begin
+      $monitor("a=%0d,b=%0d,xor(Y)=%0d",a,b,y);
+        a = 0; b = 0; #10;
+        a = 0; b = 1; #10;
+        a = 1; b = 0; #10;
+        a = 1; b = 1; #10;
+        $finish;
+    end
+endmodule
+//output:
+# KERNEL: a=0,b=0,xor(Y)=0
+# KERNEL: a=0,b=1,xor(Y)=1
+# KERNEL: a=1,b=0,xor(Y)=1
+# KERNEL: a=1,b=1,xor(Y)=0
