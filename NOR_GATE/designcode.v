e3d826f12b075ff8d9c4970a18cd6c9f4464b728
K.Suvarna Subramaneswari <166210217+Suvarnasetty10@users.noreply.github.com>
2025-09-20 13:32:41 +0530

    designcode.v

---
+++ NOR_GATE/designcode.v
@@ -0,0 +1,11 @@
+module nor_gate_using_mux (
+    input wire a,
+    input wire b,
+    output wire y
+);
+    wire I0; 
+    wire I1;
+    assign I0 = ~b;
+    assign I1 = 1'b0;
+  assign y = (~a & I0) | (a & I1);
+endmodule
